bec97ca581668446fbe13574b5f2f3e434a6575e
100130098 <jjhill080103@gmail.com>
2019-10-24 08:35:16 -0500

    Repetition and concatenation

---
+++ StringsAndThings.py
@@ -0,0 +1,20 @@
+# strings are anything that falls within "  "
+
+# Concatenation: put two strings together
+
+firstName = "Fred"
+lastName = "Flintstone"
+
+fullName = (firstName +" " + lastName)
+
+print(fullName)
+
+# Repetition operator is the multiplier: *
+print("Hip "*2 + "Hooray")
+def rowYourBoat():
+    print("Row "*3 + "your boat")
+    print("gently down the stream")
+    print("merrily, "*4)
+    print("life is but a dream")
+
+print(rowYourBoat())
