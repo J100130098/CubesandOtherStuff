1cd512699f95c0caab14d2c525ac913163af5eb6
100130098 <jjhill080103@gmail.com>
2019-10-29 08:43:14 -0500

    Added crypto stuff

---
+++ crypto.py
@@ -0,0 +1,16 @@
+# transposition cipher
+
+# original This_is_a_secret_message_i_want_to_transmit_
+# encrypted hsi_ertmsaera_att_rnmtti_sasce_esg_htiwn_otasi
+def scramble2Encrypt(plainText):
+    evenChars = ""
+    oddChars = ""
+    charCount = 0
+    for ch in plainText:
+        if charCount % 2 == 0
+            evenChars = evenChars + ch
+        else:
+            oddChars = oddChars + ch
+        charCount = charCount + 1
+    cipherText = oddChars + evenChars
+    return cipherText
